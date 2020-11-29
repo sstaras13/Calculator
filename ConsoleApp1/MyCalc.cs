8fe93a0e12669de60d97d9581f7e94f006a066c4
sstaras13 <58522766+sstaras13@users.noreply.github.com>
2020-11-29 22:35:33 +0300

    Add files via upload

---
+++ ConsoleApp1/MyCalc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+     class MyCalc
+    {
+        double a;
+        double b;
+        public string oper;
+        public double res;
+
+        public void GetData()
+        {
+            Console.Write("Please enter the first number:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Now enter the second number:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter an operation +, -, * or /:");
+            oper = Console.ReadLine();
+        }
+
+        public double Add() => res = a + b;
+
+        public double Subtruct() => res = a - b;
+
+        public double Devide() => res = a / b;
+
+        public double Multiply() => res = a * b;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCalc myCalc = new MyCalc();
+            myCalc.GetData();
+            switch (myCalc.oper)
+            {
+                case "+":
+                    myCalc.Add();
+                    break;
+                case "-":
+                    myCalc.Subtruct();
+                    break;
+                case "/":
+                    myCalc.Devide();
+                    break;
+                case "*":
+                    myCalc.Multiply();
+                    break;
+                default:
+                    Console.WriteLine("Wrong operation!");
+                    break;
+            }
+            Console.WriteLine($"The result is: {myCalc.res}");
+            //Console.ReadKey();
+        }
+    }
+}
